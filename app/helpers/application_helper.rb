a4a8e1594dd4709d7873b897132cf848f5f5abda
Ishaan Sharma <Unkn0wn@Ishaan.local>
2016-05-26 16:13:42 +0530

    added pg and rails 12 factor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,12 @@ module ApplicationHelper
 		renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
 		options = {
 			autolink: true,
-		      no_intra_emphasis: true,
-		      disable_indented_code_blocks: true,
-		      fenced_code_blocks: true,
-		      lax_html_blocks: true,
-		      strikethrough: true,
-		      superscript: true
+		    no_intra_emphasis: true,
+		    disable_indented_code_blocks: true,
+		    fenced_code_blocks: true,
+		    lax_html_blocks: true,
+		    strikethrough: true,
+		    superscript: true
 		}
 		Redcarpet::Markdown.new(renderer, options).render(content).html_safe
 	end
