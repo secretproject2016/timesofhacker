2e239ce3d515c1b130a3cd28efb0a4fb1d8cd4bf
Ishaan Sharma <Unkn0wn@Ishaan.local>
2016-05-26 18:26:57 +0530

    friendly id for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,7 +41,7 @@ class ProjectsController < ApplicationController
 	private
 
 	def find_project
-		@project = Project.find(params[:id])
+		@project = Project.friendly.find(params[:id])
 	end
 
 	def project_params
